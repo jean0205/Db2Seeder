a3e557ad209686fb800c2dfbb1b4171e87312cf0
Jean Carlos Soto <jeanc.soto020@gmail.com>
2021-11-13 15:11:07 -0400

    crashes report

--- Db2Seeder/Program.cs
+++ Db2Seeder/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,9 +18,22 @@ namespace Db2Seeder
         [STAThread]
         static void Main()
         {
+            Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            Analytics.SetEnabledAsync(true);
+            AppCenter.Start("18969fc7-c640-4544-8fe7-09542a100eae",
+                   typeof(Analytics), typeof(Crashes));
+
+
             Application.Run(new Form1());
+        }
+        static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            Crashes.TrackError(e.Exception);
+           
+
         }
     }
 }
