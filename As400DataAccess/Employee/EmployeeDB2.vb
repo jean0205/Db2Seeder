be7d9d733b0ad968e26cda8823acde34126e5f5c
Sarahi Palacio <spalacio@nisgrenada.org>
2021-11-13 09:54:42 -0400

    xx

--- As400DataAccess/Employee/EmployeeDB2.vb
+++ As400DataAccess/Employee/EmployeeDB2.vb
@@ -30,11 +30,89 @@ Public Class EmployeeDB2
             Throw ex
         End Try
 
+        Return total
+    End Function
 
 
 
+    Async Function InsertEmployees() As Task(Of Integer)
+
+        Dim total As Integer = 0
+        Try
+            Using connection As New iDB2Connection(cn)
+                Dim cmd As New iDB2Command()
+                connection.Open()
+                Dim rs As iDB2DataReader
+                cmd.CommandText = "SELECT  count(*)
+	                                  FROM ""QS36F"".""" & As400_lib & ".EMPE"" E                               
+                                WHERE  DATD03 = 0 and DATB03 <> 0 and ACTV03 = 'A'"
+                cmd.Connection = connection
+                cmd.CommandTimeout = 0
+                cmd.DeriveParameters()
+                rs = Await cmd.ExecuteReaderAsync
+                If rs.Read Then
+                    total = rs(0)
+                End If
+                cmd.Dispose()
+                rs.Close()
+            End Using
+        Catch ex As iDB2Exception
+            Throw ex
+        End Try
 
         Return total
+    End Function
+
+    Private Function GenerarNIS() As Integer
+        Dim NISRegister As Integer = 0
+        Try
+            Using connection As New iDB2Connection(cn)
+                Dim cmd As New iDB2Command()
+
+                connection.Open()
+                Dim rs As iDB2DataReader
+                cmd.CommandText = "SELECT * FROM ""QS36F"".""" & As400_lib & ".CONS""                                
+                                WHERE CKEY01 LIKE 'Z  005' "
+                cmd.Connection = connection
+                cmd.CommandTimeout = 0
+                cmd.DeriveParameters()
+                rs = cmd.ExecuteReader
+                If rs.Read Then
+                    Dim NISNUMBER As String
+                    NISNUMBER = Checkdigitcalc(Convert.ToInt32(rs("DATA01").ToString.Substring(30, 9))).ToString
+                    NISRegister = CInt(NISNUMBER)
+
+                    'UPDATE CONS FILE
+                    Dim cmdupd As New iDB2Command With {
+                        .CommandText = "UPDATE ""QS36F"".""" & As400_lib & ".CONS""                                
+                                SET DATA01 = @DATA WHERE CKEY01 LIKE @CKEY ",
+                        .Connection = connection,
+                        .CommandTimeout = 0
+                    }
+                    cmdupd.DeriveParameters()
+
+                    Dim strdata As String
+                    Dim newnis As Integer = Convert.ToInt32(NISNUMBER) + 10
+                    Dim newnisstr As String = Convert.ToString(newnis)
+                    While newnisstr.Length < 9
+                        newnisstr = "0" + newnisstr
+                    End While
+                    strdata = Mid(rs("DATA01"), 1, 30) + newnisstr
+
+                    cmdupd.Parameters("@DATA").Value = strdata
+                    cmdupd.Parameters("@CKEY").Value = "Z  005"
+                    cmdupd.ExecuteNonQuery()
+                    cmdupd.Dispose()
+
+                End If
+                cmd.Dispose()
+                rs.Close()
+            End Using
+        Catch ex As iDB2Exception
+            Throw ex
+        End Try
+
+        Return NISRegister
     End Function
 
 End Class
---
+++ As400DataAccess/Employee/ModHelps.vb
@@ -0,0 +1,62 @@
+﻿Module ModHelps
+    Public Function Checkdigitcalc(ByVal numb As Integer) As Integer
+        Dim Result As Integer = 0
+        Dim num1, num2, num3, num4, num5, num6, num7, num8, num9 As Integer
+        Dim sum1a, sum2a, sum3a, sum4a, sum5a, sum1b, sum2b, sum3b, sum4b, sum5b As Integer
+        Dim wrka, wrka2, sum1t, sum2t, sum3t, sum4t, sum5t As Integer
+        Dim top, bottom, prechk, calc1, prechk1, prechk2, chkdig As Integer
+        num1 = (numb \ 100000000)
+        num2 = (numb \ 10000000) Mod 10
+        num3 = (numb \ 1000000) Mod 10
+        num4 = (numb \ 100000) Mod 10
+        num5 = (numb \ 10000) Mod 10
+        num6 = (numb \ 1000) Mod 10
+        num7 = (numb \ 100) Mod 10
+        num8 = (numb \ 10) Mod 10
+        num9 = numb Mod 10
+        'step 1
+
+        sum1a = 0
+        sum2a = num2
+        sum3a = num4
+        sum4a = num6
+        sum5a = num8
+
+        wrka = sum1a * 10000 + sum2a * 1000 + sum3a * 100 + sum4a * 10 + sum5a
+        wrka2 = wrka * 2
+
+        sum1t = (wrka2 \ 10000)
+        sum2t = (wrka2 \ 1000) Mod 10
+        sum3t = (wrka2 \ 100) Mod 10
+        sum4t = (wrka2 \ 10) Mod 10
+        sum5t = wrka2 Mod 10
+
+        top = sum1t + sum2t + sum3t + sum4t + sum5t
+
+        ' step 2
+        sum1b = 0
+        sum2b = num1
+        sum3b = num3
+        sum4b = num5
+        sum5b = num7
+
+        bottom = sum1b + sum2b + sum3b + sum4b + sum5b
+
+        ' step 3
+        prechk = top + bottom
+        prechk1 = prechk \ 10
+        calc1 = (prechk1 + 1) * 10
+
+        prechk2 = calc1 - prechk
+
+        If prechk2 > 9 Then
+            chkdig = prechk2 - (prechk2 \ 10) * 10
+        Else
+            chkdig = prechk2
+        End If
+        Result = 100000000 * num1 + 10000000 * num2 + 1000000 * num3 + 100000 * num4 + 10000 * num5 + 1000 * num6 + 100 * num7 + 10 * num8 + chkdig
+        Checkdigitcalc = Result
+    End Function
+
+
+End Module
