72e8b56fe5ab4ceca2d7f909c89f3f0e9f3c68aa
Jean Carlos Soto <jeanc.soto020@gmail.com>
2021-11-15 10:26:07 -0400

    dss

---
+++ Db2Seeder/NIS.SQL.Documents/Models_ScannedDocuments/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Db2Seeder.NIS.SQL.Documents.Models_ScannedDocuments
+{
+    internal class Class1
+    {
+    }
+}
