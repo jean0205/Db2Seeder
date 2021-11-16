76f35330220bc022a497a6c9f935bda842a7ca7a
Jean Carlos Soto <jeanc.soto020@gmail.com>
2021-11-16 15:43:17 -0400

    dfdgdg

--- Db2Seeder/Business/Remittance.cs
+++ Db2Seeder/Business/Remittance.cs
@@ -1,4 +1,5 @@
-﻿using Db2Seeder.API.Helpers;
+﻿using As400DataAccess;
+using Db2Seeder.API.Helpers;
 using Db2Seeder.API.Models;
 using Db2Seeder.API.Request;
 using Microsoft.AppCenter.Crashes;
@@ -13,6 +14,7 @@ namespace Db2Seeder.Business
 {
     public class Remittance
     {
+        ElectRemittanceDB2 as400Remittance;
         List<SupportRequest> RequestList;
         Document_Remittance Document_Remittance;
         //SupportRequest/GetByState/Type/15/State/9
@@ -34,30 +36,50 @@ namespace Db2Seeder.Business
                             Document_Remittance = new Document_Remittance();
                             Document_Remittance = await GetRequestDetailsRemittance(guid);
                             string json = JsonConvert.SerializeObject(Document_Remittance);
-                            var RemittanceCopy= JsonConvert.DeserializeObject<Document_Remittance>(json);
-
+                            var RemittanceCopy = JsonConvert.DeserializeObject<Document_Remittance>(json);
                             var periods = Document_Remittance.employeeContributionRecords.Select
                                 (x => new { x.contributionPeriodYear, x.contributionPeriodMonth }).Distinct().ToList();
 
                             foreach (var period in periods)
                             {
+                                //pasando un solo periodo a la vez
                                 RemittanceCopy.employeeContributionRecords.Clear();
+                                RemittanceCopy.employeeContributionRecords = Document_Remittance.employeeContributionRecords.Where(x => x.contributionPeriodMonth == period.contributionPeriodMonth && x.contributionPeriodYear == period.contributionPeriodYear).ToList();
 
-                                RemittanceCopy.employeeContributionRecords= Document_Remittance.employeeContributionRecords.Where(x=>x.contributionPeriodMonth==period.contributionPeriodMonth && x.contributionPeriodYear==period.contributionPeriodYear).ToList();
+                                //copiando los earning para la ultima semana trabajada
+                                foreach (var employee in RemittanceCopy.employeeContributionRecords.Where(x=>x.frequency=="M"))
+                                {
+                                    if (employee.week5.hasWorked == true)
+                                    {
+                                        employee.week5.amount = employee.insurableEarnings;
+                                        goto jmp;
+                                    }
+                                    if (employee.week4.hasWorked == true)
+                                    {
+                                        employee.week4.amount = employee.insurableEarnings;
+                                        goto jmp;
+                                    }
+                                    if (employee.week3.hasWorked == true)
+                                    {
+                                        employee.week3.amount = employee.insurableEarnings;
+                                        goto jmp;
+                                    }
+                                    if (employee.week2.hasWorked == true)
+                                    {
+                                        employee.week2.amount = employee.insurableEarnings;
+                                        goto jmp;
+                                    }
+                                    employee.week1.amount = employee.insurableEarnings;
+                                jmp:;
+                                }                            
                                 //toma palacio
-
-
+                                as400Remittance = new ElectRemittanceDB2();
+                                await as400Remittance.PostRemittances(RemittanceCopy);
                             }
-
-
-
-
-
-
-
                         }
                     }
                 }
+
             }
             catch (Exception ex)
             {
--- Db2Seeder/Form1.Designer.cs
+++ Db2Seeder/Form1.Designer.cs
@@ -35,13 +35,18 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 415);
+            this.button1.Location = new System.Drawing.Point(4, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(112, 32);
             this.button1.TabIndex = 0;
             this.button1.Text = "employee";
             this.button1.UseVisualStyleBackColor = true;
@@ -49,9 +54,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(102, 415);
+            this.button2.Location = new System.Drawing.Point(231, 4);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(112, 32);
             this.button2.TabIndex = 1;
             this.button2.Text = "employer";
             this.button2.UseVisualStyleBackColor = true;
@@ -69,9 +75,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(183, 415);
+            this.button3.Location = new System.Drawing.Point(458, 4);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(112, 32);
             this.button3.TabIndex = 2;
             this.button3.Text = "Remittance";
             this.button3.UseVisualStyleBackColor = true;
@@ -79,26 +86,64 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(275, 415);
+            this.button4.Location = new System.Drawing.Point(685, 4);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(102, 23);
+            this.button4.Size = new System.Drawing.Size(153, 32);
             this.button4.TabIndex = 3;
             this.button4.Text = "Compliance Cert.";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 91.49233F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.50767F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1369, 717);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 6;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button4, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button2, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button3, 2, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 659);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1363, 55);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(33)))), ((int)(((byte)(53)))));
+            this.ClientSize = new System.Drawing.Size(1369, 717);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -111,6 +156,8 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
     }
 }
 
