f1029b328890d112359d3c3270d8d6719ca9b49f
Jean Carlos Soto <jeanc.soto020@gmail.com>
2023-02-17 14:40:51 -0400

    live

--- ShareModels/Helpers/Settings.cs
+++ ShareModels/Helpers/Settings.cs
@@ -5,7 +5,7 @@
         public static string GetPortalUrl()
         {
             return "https://my.nisgrenada.org/SupportRequest/Detail/";
-            //return "http://my-nis-uat.loteklabs.com/SupportRequest/Detail/";
+           // return "http://my-nis-uat.loteklabs.com/SupportRequest/Detail/";
 
         }      
        
