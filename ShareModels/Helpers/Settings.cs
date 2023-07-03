170228f790c90e391f55fe2c75400f5deba6dd16
Jean Carlos Soto <jeanc.soto020@gmail.com>
2023-07-03 11:29:50 -0400

    testing

--- ShareModels/Helpers/Settings.cs
+++ ShareModels/Helpers/Settings.cs
@@ -1,13 +1,13 @@
 ﻿namespace ShareModels.Helpers
 {
     public class Settings
-    {       
+    {
         public static string GetPortalUrl()
         {
-         return "https://my.nisgrenada.org/SupportRequest/Detail/";
-         // return "http://my-nis-uat.loteklabs.com/SupportRequest/Detail/";
+            //return "https://my.nisgrenada.org/SupportRequest/Detail/";
+            return "http://my-nis-uat.loteklabs.com/SupportRequest/Detail/";
 
-        }      
-       
+        }
+
     }
 }
